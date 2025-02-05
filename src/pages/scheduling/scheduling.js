e66d2aa393f49a733f3eb0adb72f285021abe534
Lux <luxetnoxesports@gmail.com>
2025-02-05 13:38:45 -0700

    Moved phone-type-formatter.us.js script tag to scheduling.js

--- src/pages/scheduling/scheduling.js
+++ src/pages/scheduling/scheduling.js
@@ -4,3 +4,4 @@ import "./css/scheduling.css";
 // Importing JavaScript files
 import "./scripts/appointmentForm.js";
 import "./scripts/scrollToTop.js";
+import "../../components/js/phone-type-formatter.us.js";
