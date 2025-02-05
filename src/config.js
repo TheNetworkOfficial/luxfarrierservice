8a36f20170ff67e264603b5a03c5af550fcf6326
Lux <luxetnoxesports@gmail.com>
2025-02-05 13:51:00 -0700

    Changed config API_BASE_URL arguments

--- src/config.js
+++ src/config.js
@@ -2,5 +2,5 @@
 const isProduction = process.env.NODE_ENV === 'production';
 
 export const API_BASE_URL = isProduction
-  ? 'https://api.luxfarrierservice.com'
+  ? ''
   : 'http://localhost:3000';
